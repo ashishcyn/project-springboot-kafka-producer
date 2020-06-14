6b7d060f45b8c0db8396b5de57f190ead381f075
Ashish <c2nashish@gmail.com>
2020-06-14 15:49:31 +0400

    01.Springboot project

---
+++ src/main/java/com/ashi/tech/projectspringbootkafkaproducer/ProjectSpringbootKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ashi.tech.projectspringbootkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootKafkaProducerApplication.class, args);
+	}
+
+}
